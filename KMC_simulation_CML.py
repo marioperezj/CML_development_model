1ac03cee2236eae65bb92b0f45f8c76398f9862f
marioperezj <marioperez@ciencias.unam.mx>
2018-11-19 09:40:17 +0100

    Including iterations to find the correct rates

--- KMC_simulation_CML.py
+++ KMC_simulation_CML.py
@@ -17,34 +17,41 @@ cou=0 #This variable will be a counter of how many iteratios are in the simulati
 l=0 # This is a loop that allow us to run the same simulation many times to get the necessary statistics
 #while l<1: #loop that creates many instances of the iteration
 #    l=l+1 #updating the index of the loop
+gamma=5
 n=5  #NUmber of levels in the hierarchy
-events=3 #Number of different events happening in the hierarchy for instance symetric cell division, death, mutation,etc.
-t=np.zeros((n*events,events)) #Initializing the static array that will storage the levels and distinct rates of each one
+events=6 #Number of different events happening in the hierarchy for instance symetric cell division, death, mutation,etc.
+t=np.zeros((n*events,3)) #Initializing the static array that will storage the levels and distinct rates of each one
 
 for i in np.arange(n): #This procedure creates the array in first column is the number of level
     for k in np.arange(1,events+1): #In the second column it will be the number of rate for each level
-        t[3*i+k-1:3*i+k,1]=k #The third column is the value of each one of the rates
-        t[3*i+k-1:3*i+k,0]=i+1
+        t[events*i+k-1:events*i+k,1]=k #The third column is the value of each one of the rates
+        t[events*i+k-1:events*i+k,0]=i+1
         
-c=np.zeros((n,2)) #Initializing the array that storage that will storage the number of cells in each level
-c[0,0]=10 #Also initiaties the number of cells in each level, also the total differentiation rate per level
+c=np.ones((n,2)) #Initializing the array that storage that will storage the number of cells in each level
+c[0,0]=100 #Also initiaties the number of cells in each level, also the total differentiation rate per level
 
-t[0,2]=0.5 #initializing the different rates for each one of the levels in the hierarchy.
-t[1,2]=0.5
 t[3,2]=0.5
-t[4,2]=0.5
-t[5,2]=0
-t[6,2]=0.5
-t[7,2]=0.5 
-t[8,2]=0
-t[9,2]=0.5
-t[10,2]=0.5
-t[11,2]=0
-t[12,2]=0
-t[14,2]=1
+t[4,2]=0.5 #initializing the different rates for each one of the levels in the hierarchy
+t[n*events-1,2]=2.0
+t[(n-1)*events-1,2]=1
+t[n*events-3,2]=0
+
+for i in np.arange(2,n):
+    t[((n-i)*events)-1,2]=t[(n-(i-1))*events-1,2]/gamma
+    
+for i in np.arange(2,n):
+    t[(i)*events-3,2]=0.7
+    
+for i in np.arange(1,n-1):
+    t[(i+1)*events-2,2]=(2)*(t[(i)*events-1,2]/t[(i+1)*events-1,2])/t[(i)*events-3,2]
+#    t[(i+1)*events-3,2]=(2)*(t[(i)*events-1,2]/t[(i+1)*events-1,2])/t[(i)*events-2,2]
+    
+for i in np.arange(0,n):
+    t[i*events+1,2]=0.5*t[(i+1)*events-1,2]*t[i*events+3,2]
+    t[i*events,2]=0.5*t[(i+1)*events-1,2]*t[i*events+3,2]*(1-t[(i+1)*events-2,2])
 
 x=0 #Initializing the index for the Monte Carlo algorithm.
-while x<100: #Stop the Kinetic Monte Carlo algorithm after a definite time. 
+while x<100000: #Stop the Kinetic Monte Carlo algorithm after a definite time. 
     
     if np.amax(c)==0: #Computing the sum of the total number of cells WRONG!!!
         break
@@ -62,9 +69,10 @@ while x<100: #Stop the Kinetic Monte Carlo algorithm after a definite time.
          
     for i in np.arange(n):#This procedure creates the array in first column is the number of level THIS IS FOR THE DYNAMIC ARRAY 
         for k in np.arange(1,events+1): #In the second column it will be the number of rate for each level CHANGING IN EACH ITERATION
-            k_m[3*i+k-1:3*i+k,2]=(c[i,1]*t[3*i+k-1:3*i+k,2]) #The third column is the value of each one of the rates
+            k_m[events*i+k-1:events*i+k,2]=c[i,1]*t[events*i+k-1:events*i+k,2]#The third column is the value of each one of the rates
     
     k_m=k_m[k_m[:,2]!=0] #Choosing only non zero transition rate for different states    
+    k_m=k_m[(k_m[:,1]==1)|(k_m[:,1]==2)]
             
     cum_sum=np.cumsum(k_m[:,2],axis=0).reshape(np.shape(k_m)[0],1)
     
@@ -97,8 +105,9 @@ while x<100: #Stop the Kinetic Monte Carlo algorithm after a definite time.
         
     ran=np.random.uniform(0,1,1) #Generating a new random number for updating the time in the KMC algorithm
     x=x+np.log(1/ran)/max_list #Computing the udpate in time
+    pri_array=np.append(np.array([x]).reshape(1,1),c[:,0].reshape(1,n),axis=1)
     with open('out.txt', 'a') as f: #Open the file to save the results
-                np.savetxt(f,c[:,0].reshape(1,n),fmt='%-17d') #Saving the results as a table with 17 postions and numbers as integers
+                np.savetxt(f,pri_array.reshape(1,n+1),fmt='%5.10f') #Saving the results as a table with 17 postions and numbers as integers
 print(cou) #Print in the terminal the total number of iterations in the simulation
 print("--- %s seconds ---" % (time.time() - start_time)) #Print the total time of the simulation
 #print(c)
